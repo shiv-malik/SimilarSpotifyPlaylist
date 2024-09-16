8f3f40530da80446cb97fb3e5b890a9c043464e4
Shiv Malik <shivmalik126+school@gmail.com>
2024-09-15 17:13:50 -0700

    backend class

--- server/main.py
+++ server/main.py
@@ -1,26 +1,11 @@
-from spotify import sp, getPlaylistID, getTracks, findSimilarTracks
+from spotify import SimilarSpotifyPlaylist
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 if __name__ == '__main__':
-    playlist = getPlaylistID('https://open.spotify.com/playlist/1Hs11dJy0ntKeUCOXVCvRx?si=827158cefeea40e3')
-    
-    sourceLength = sp.playlist_items(playlist, fields='total')['total']
-    newPlaylistName = input('playlist name: ')
-    ratio = int(input('ratio: '))
-    # targetLength = 50
-    # ratio = targetLength / sourceLength
+    playlistLink = input('playlist link: ')
+    newPlaylistName = input('new playlist name: ')
+    ratio = int(input('ratio (new playlist length / old playlist length): '))
 
-
-    trackGenerator = getTracks(playlist, sourceLength)
-    user = sp.current_user()['id']
-    newPlaylist = sp.user_playlist_create(user=user, name=newPlaylistName, public=True)['id']
-
-    while True:
-        try:
-            tracks = next(trackGenerator)
-            similarTracks = findSimilarTracks(tracks, ratio)
-            
-            sp.playlist_add_items(playlist_id=newPlaylist, items=similarTracks)
-
-
-        except StopIteration:
-            break
+    spotify = SimilarSpotifyPlaylist(playlistLink, newPlaylistName, ratio)
+    spotify.createNewPlaylist()
